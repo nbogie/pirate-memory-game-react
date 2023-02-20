c5df7c02f3efd78e633015357f6e02daf9125845
nbogie <neillbogie@googlemail.com>
2023-02-20 02:57:56 +0000

    initial commit - yuckky cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -7,28 +6,45 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <PirateMemoryGame />
     </div>
   )
 }
 
+function PirateMemoryGame() {
+  const deck = createDeck();
+  return <div className="game">
+    <div className="cardGrid">
+      {deck.map(c => (
+        <div className={`card ${c.creature} ${c.backing} faceDown`}>{c.creature}</div>
+      ))}
+    </div>
+  </div >
+}
 export default App
+
+const allBackings = ["lava", "water", "desert", "flowers", "jungle"] as const;
+export type Backing = typeof allBackings[number];
+const allCreatures = ["🐧", "🐢", "🐙", "🐋", "🦀"] as const;
+export type Creature = typeof allCreatures[number];
+export interface Card {
+  backing: Backing;
+  creature: Creature;
+}
+
+function createDeck() {
+  const cards: Card[] = [];
+  for (const backing of allBackings) {
+    for (const creature of allCreatures) {
+      const c: Card = { backing, creature };
+      cards.push(c)
+    }
+  }
+  const shuffled = shuffle(cards);
+  shuffled.pop();
+  return shuffled;
+}
+
+function shuffle<T>(arr: T[]): T[] {
+  return [...arr.sort(() => Math.random() < 0.5 ? -1 : 1)];
+}
