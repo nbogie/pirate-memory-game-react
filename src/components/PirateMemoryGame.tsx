7758ec2a8c5ca22878c3fda154c5e585914b9c83
nbogie <neillbogie@googlemail.com>
2023-02-21 22:18:22 +0000

    add sounds - manually triggered for now

--- src/components/PirateMemoryGame.tsx
+++ src/components/PirateMemoryGame.tsx
@@ -6,6 +6,10 @@ import { CardView } from "./CardView";
 import { GameOverView } from "./GameOverView";
 import { PlayersAtTableEdges } from "./PlayersAtTableEdges";
 import { TreasureCardView } from "./TreasureCardView";
+import useSound from 'use-sound';
+import goodNotes from "../assets/goodNotes.wav";
+import wrongNotes from "../assets/wrongNotes.wav";
+import { pick } from "../utils/pick";
 // import { OLDPlayersAtTableMini } from "./OLDPlayersAtTableMini";
 
 interface PirateMemoryGameProps {
@@ -13,6 +17,26 @@ interface PirateMemoryGameProps {
 }
 
 export function PirateMemoryGame(props: PirateMemoryGameProps) {
+    const [playGoods] = useSound(goodNotes, {
+        sprite:
+        {
+            good1: [0, 2500],
+            good2: [2500, 2500],
+            good3: [5040, 2500],
+            good4: [7555, 2500],
+            good5: [10090, 2500],
+            good6: [12620, 2500]
+        }
+    });
+    const [playBads] = useSound(wrongNotes, {
+        sprite:
+        {
+            bad1: [0, 2400],
+            bad2: [2430, 2090],
+            bad3: [4530, 2530],
+            bad4: [7088, 2700],
+        }
+    });
     const initialGameState = createInitialGameState(props.numPlayers);
     const [gameState, dispatch] = useReducer(reducerFunction, initialGameState);
 
@@ -59,6 +83,8 @@ export function PirateMemoryGame(props: PirateMemoryGameProps) {
             </div>
             <div>
                 <button onClick={() => dispatch({ type: "cheat-set-game-over" })}>Set Game Over</button>
+                <button onClick={() => playGoods({ id: "good" + pick([1, 2, 3, 4, 5, 6]) })}>Play Good</button>
+                <button onClick={() => playBads({ id: "bad" + pick([1, 2, 3, 4]) })}>Play Bad</button>
                 {/* <OLDPlayersAtTableMini gameState={gameState} /> */}
                 <div>{gameState.roundPhase.type}</div>
 
