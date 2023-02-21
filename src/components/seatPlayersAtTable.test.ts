1acadf86c4c9e2434c25f44ca07450f0e0f92bbb
nbogie <neillbogie@googlemail.com>
2023-02-21 20:51:03 +0000

    tweak seating fns

--- src/components/seatPlayersAtTable.test.ts
+++ src/components/seatPlayersAtTable.test.ts
@@ -2,7 +2,6 @@ import { expect, test } from 'vitest';
 import { seatPlayersAtTable } from "./seatPlayersAtTable";
 
 test("seatPlayersAtTable", () => {
-    expect(3 + 4).toEqual(7);
     expect(seatPlayersAtTable(["one", "two"])).toEqual(["one", null, "two", null]);
     expect(seatPlayersAtTable(["one", "two", "three"])).toEqual(["one", "two", null, "three"]);
     expect(seatPlayersAtTable(["one", "two", "three", "four"])).toEqual(["one", "two", "three", "four"]);
--- src/components/seatPlayersAtTable.tsx
+++ src/components/seatPlayersAtTable.tsx
@@ -1,3 +1,8 @@
+/** Given an array of 2, 3, or 4 players (really, things), sit them appropriately at 
+ * the N, E, S, W sides of the table by placing them in a returned array at those positions.  
+ * Empty seats will be marked with null. 
+ * 
+ * @returns a tuple of 4 items or nulls, to be interpreted as the seating plan in order: N, E, S, W */
 export function seatPlayersAtTable<T>(players: T[]): [T | null, T | null, T | null, T | null] {
     const [a, b, c, d] = players;
     switch (players.length) {
