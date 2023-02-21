7cabf9e8ab5c5a21fab75705356ac8c078b0843f
nbogie <neillbogie@googlemail.com>
2023-02-21 00:24:03 +0000

    react-spring card-flip anim

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -1,5 +1,6 @@
 import { Action } from "../gameCore/action";
 import { Card } from "./Card";
+import { animated, useSpring } from '@react-spring/web'
 
 interface CardViewProps {
     card: Card;
@@ -8,14 +9,18 @@ interface CardViewProps {
     isPreviousFlip: boolean;
 }
 export function CardView({ card, dispatch, isLatestFlip, isPreviousFlip }: CardViewProps): JSX.Element {
+    const springs = useSpring({
+        transform: `perspective(600px) rotateX(${card.isFaceUp ? 0 : 180}deg)`
+    });
     const facingClass = card.isFaceUp ? "faceUp" : "faceDown";
     const highlightClass = isLatestFlip ? "latest" : (isPreviousFlip ? "previous" : "");
     return (
-        <div
+        <animated.div
+            style={{ ...springs }}
             className={`card ${card.creature} ${card.backing} ${facingClass} ${highlightClass}`}
             onClick={() => dispatch({ type: "flip", clickedCard: card })}
         >
             {card.isFaceUp ? card.creature : ""}
-        </div>
+        </animated.div>
     );
 }
