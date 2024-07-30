ae48362d701c800a46117fbe9d094cadfe0a6ba2
nbogie <neillbogie@googlemail.com>
2024-07-30 12:12:40 +0100

    fix treasure card view so useSpring is called unconditionally.

--- src/components/TreasureCardView.tsx
+++ src/components/TreasureCardView.tsx
@@ -10,25 +10,25 @@ interface TreasureCardViewProps {
 export function TreasureCardView(props: TreasureCardViewProps) {
 
     const { gameState, dispatch } = props;
-    if (gameState.roundPhase.type !== "round-end") {
-        return <div>ERROR: TreasureCardView expects round-end but got {gameState.roundPhase.type} </div>
+
+    const _alternativeUnusedConfig = {
+        from: { rotateZ: -10 },
+        to: { rotateZ: 0 },
+        config: { damping: 0.1, frequency: 0.2 }
     }
-
-    const winnerIx = gameState.roundPhase.winnerIx;
-    // const winner = getPlayerByPosIndex(gameState, winnerIx);
-
-    // const springs = useSpring({
-    //     from: { rotateZ: -10 },
-    //     to: { rotateZ: 0 },
-    //     config: { damping: 0.1, frequency: 0.2 }
-    // })
-
+    
     const swellSprings = useSpring({
         from: { scale: 0.97 },
         to: { scale: 1 },
         config: { damping: 0, frequency: 0.8 }
     })
 
+    if (gameState.roundPhase.type !== "round-end") {
+        return <div>ERROR: TreasureCardView expects round-end but got {gameState.roundPhase.type} </div>
+    }
+
+    const winnerIx = gameState.roundPhase.winnerIx;
+    
     return (
         <animated.div
             className={`centreCard treasure`}
